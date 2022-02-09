45d37f1ca35080323b22f5cf53731d7ded7cdd71
Anatolik <disaer88@gmail.com>
2022-02-10 01:58:48 +0300

    start version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-<<<<<<< HEAD
+
 import Header from "./components/Header/Header";
 //import FirstPart from "./components/FirstPart/FirstPart";
 //import SecondPart from "./components/SecondPart/SecondPart";
@@ -8,42 +8,25 @@ import Header from "./components/Header/Header";
 //import FourthPart from "./components/FourthPart/FourthPart";
 //import FifthPart from "./components/FifthPart/FifthPart";
 //import SixthPart from "./components/SixthPart/SixthPart";
-=======
-// import Header from "./components/Header/Header";
-// import FirstPart from "./components/FirstPart/FirstPart";
-// import SecondPart from "./components/SecondPart/SecondPart";
-// import ThirdPart from "./components/ThirdPart/ThirdPart";
-// import FourthPart from "./components/FourthPart/FourthPart";
-// import FifthPart from "./components/FifthPart/FifthPart";
-// import SixthPart from "./components/SixthPart/SixthPart";
->>>>>>> dd866e96b57bc1e5dc8fbb7b00ba0b4772a00a51
 
 import "./App.css";
 
 function App() {
-  return (
-    <div>
+    return (
+        <div>
 
-       <h1>Hello world</h1>
-<<<<<<< HEAD
-      <Header />
-      {/*<FirstPart />
+            <h1>Hello world</h1>
+
+            <Header/>
+            {/*<FirstPart />
       <SecondPart />
       <ThirdPart />
       <FourthPart />
       <FifthPart />
       <SixthPart />*/}
-=======
-      {/*<Header />*/}
-      {/*<FirstPart />*/}
-      {/*<SecondPart />*/}
-      {/*<ThirdPart />*/}
-      {/*<FourthPart />*/}
-      {/*<FifthPart />*/}
-      {/*<SixthPart />*/}
->>>>>>> dd866e96b57bc1e5dc8fbb7b00ba0b4772a00a51
-    </div>
-  );
+
+        </div>
+    );
 }
 
 export default App;
