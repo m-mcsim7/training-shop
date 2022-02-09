19479b95e257d50312fe904307134ea4b142be30
m-mcsim7 <m-mcsim7@yandex.by>
2022-02-10 00:02:34 +0300

    del

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from "react";
 
-import Header from "./components/Header/Header";
-import FirstPart from "./components/FirstPart/FirstPart";
-import SecondPart from "./components/SecondPart/SecondPart";
-import ThirdPart from "./components/ThirdPart/ThirdPart";
-import FourthPart from "./components/FourthPart/FourthPart";
-import FifthPart from "./components/FifthPart/FifthPart";
-import SixthPart from "./components/SixthPart/SixthPart";
+//import Header from "./components/Header/Header";
+//import FirstPart from "./components/FirstPart/FirstPart";
+//import SecondPart from "./components/SecondPart/SecondPart";
+//import ThirdPart from "./components/ThirdPart/ThirdPart";
+//import FourthPart from "./components/FourthPart/FourthPart";
+//import FifthPart from "./components/FifthPart/FifthPart";
+//import SixthPart from "./components/SixthPart/SixthPart";
 
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <Header />
+
+       <h1>Hello world</h1>
+      {/*<Header />
       <FirstPart />
       <SecondPart />
       <ThirdPart />
       <FourthPart />
       <FifthPart />
-      <SixthPart />
+      <SixthPart />*/}
     </div>
   );
 }
