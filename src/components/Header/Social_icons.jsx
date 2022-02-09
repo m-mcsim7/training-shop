16b718b7648f84bf1025c2b493a657e4ffb45ddd
m-mcsim7 <m-mcsim7@yandex.by>
2022-02-09 21:10:08 +0300

    Add SecondPart

---
+++ src/components/Header/Social_icons.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+//import 'facebook' from "../../img/icons/facebook.svg";
+//import 'twitter' from "../../img/icons/twitter.svg";
+//import 'instagram' from "../../img/icons/instagram.svg";
+//import 'pinterest' from "../../img/icons/pinterest.svg";
+
+function Social_icons() {
+  return (
+   <ul className="top__social">
+   <li>
+     <a className="top__social-facebook" href="#"></a>
+   </li>
+   <li>
+     <a className="top__social-twitter" href="#"></a>
+   </li>
+   <li>
+     <a className="top__social-instagram" href="#"></a>
+   </li>
+   <li>
+     <a className="top__social-pinteerst" href="#"></a>
+   </li>
+ </ul>
+ );
+}
+
+export default Social_icons;
