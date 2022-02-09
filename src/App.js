dd866e96b57bc1e5dc8fbb7b00ba0b4772a00a51
Anatolik <disaer88@gmail.com>
2022-02-10 01:45:55 +0300

    build test 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-import Header from "./components/Header/Header";
-import FirstPart from "./components/FirstPart/FirstPart";
-import SecondPart from "./components/SecondPart/SecondPart";
-import ThirdPart from "./components/ThirdPart/ThirdPart";
-import FourthPart from "./components/FourthPart/FourthPart";
-import FifthPart from "./components/FifthPart/FifthPart";
-import SixthPart from "./components/SixthPart/SixthPart";
+// import Header from "./components/Header/Header";
+// import FirstPart from "./components/FirstPart/FirstPart";
+// import SecondPart from "./components/SecondPart/SecondPart";
+// import ThirdPart from "./components/ThirdPart/ThirdPart";
+// import FourthPart from "./components/FourthPart/FourthPart";
+// import FifthPart from "./components/FifthPart/FifthPart";
+// import SixthPart from "./components/SixthPart/SixthPart";
 
 import "./App.css";
 
@@ -15,13 +15,13 @@ function App() {
     <div>
 
        <h1>Hello world</h1>
-      <Header />
-      <FirstPart />
-      <SecondPart />
-      <ThirdPart />
-      <FourthPart />
-      <FifthPart />
-      <SixthPart />
+      {/*<Header />*/}
+      {/*<FirstPart />*/}
+      {/*<SecondPart />*/}
+      {/*<ThirdPart />*/}
+      {/*<FourthPart />*/}
+      {/*<FifthPart />*/}
+      {/*<SixthPart />*/}
     </div>
   );
 }
