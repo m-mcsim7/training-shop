0021985b87708dac503c157de81d993fb4155ce9
m-mcsim7 <m-mcsim7@yandex.by>
2022-02-10 11:03:13 +0300

    add footer without pay

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import pay1 from "../../img/icons/pay/pay1.svg";
-import pay2 from "../../img/icons/pay/pay2.svg";
-import pay3 from "../../img/icons/pay/pay3.svg";
-import pay4 from "../../img/icons/pay/pay4.svg";
-import pay5 from "../../img/icons/pay/pay5.svg";
-import pay6 from "../../img/icons/pay/pay6.svg";
-import pay7 from "../../img/icons/pay/pay7.svg";
-
+//import pay1 from "../../img/icons/pay/pay1.svg";
+//import pay2 from "../../img/icons/pay/pay2.svg";
+//import pay3 from "../../img/icons/pay/pay3.svg";
+//import pay4 from "../../img/icons/pay/pay4.svg";
+//import pay5 from "../../img/icons/pay/pay5.svg";
+//import pay6 from "../../img/icons/pay/pay6.svg";
+//import pay7 from "../../img/icons/pay/pay7.svg";
 
 import "./Footer.css";
 
@@ -27,10 +26,10 @@ function Footer() {
                 <button className="footer__input-button">Join Us</button>
               </div>
               <ul className="footer__input-social">
-                <li><a className="top__social-facebook" href="https://m-mcsim7.github.io/training-shop/"></a></li>
-                <li><a className="top__social-twitter" href="https://m-mcsim7.github.io/training-shop/"></a></li>
-                <li><a className="top__social-instagram" href="https://m-mcsim7.github.io/training-shop/"></a></li>
-                <li><a className="top__social-pinteerst" href="https://m-mcsim7.github.io/training-shop/"></a></li>
+                <li><a className="top__social-facebook" href="https://m-mcsim7.github.io/training-shop/">f</a></li>
+                <li><a className="top__social-twitter" href="https://m-mcsim7.github.io/training-shop/">f</a></li>
+                <li><a className="top__social-instagram" href="https://m-mcsim7.github.io/training-shop/">f</a></li>
+                <li><a className="top__social-pinteerst" href="https://m-mcsim7.github.io/training-shop/">f</a></li>
               </ul>
             </div>
             <div className="footer__rows">
