83540274eca8fb4ceb8bdc20ae69ac7815dce88c
m-mcsim7 <m-mcsim7@yandex.by>
2022-02-10 12:22:33 +0300

    add Categories

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 import Header from "./components/Header/Header";
-import FirstPart from "./components/FirstPart/FirstPart";
-import SecondPart from "./components/SecondPart/SecondPart";
-import ThirdPart from "./components/ThirdPart/ThirdPart";
-import FourthPart from "./components/FourthPart/FourthPart";
-import FifthPart from "./components/FifthPart/FifthPart";
-import SixthPart from "./components/SixthPart/SixthPart";
+//import FirstPart from "./components/FirstPart/FirstPart";
+//import SecondPart from "./components/SecondPart/SecondPart";
+//import ThirdPart from "./components/ThirdPart/ThirdPart";
+//import FourthPart from "./components/FourthPart/FourthPart";
+//import FifthPart from "./components/FifthPart/FifthPart";
+//import SixthPart from "./components/SixthPart/SixthPart";
+import Categories from "./components/Categories/Categories";
 import Footer from "./components/Footer/Footer";
 
 import "./App.css";
@@ -15,13 +16,15 @@ function App() {
   return (
     <div>
       <Header />
-      <FirstPart />
+      {/*<FirstPart />
       <SecondPart />
       <ThirdPart />
       <FourthPart />
       <FifthPart />
-      <SixthPart />
+      <SixthPart />*/}
+      <Categories />
       <Footer />
+
     </div>
   );
 }
