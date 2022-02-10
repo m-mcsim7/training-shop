924a12afdfed459212224b948ad2615e1c99d692
m-mcsim7 <m-mcsim7@yandex.by>
2022-02-10 09:27:09 +0300

    add first part

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
     return (
         <div>
             <Header/>
-            {/*<FirstPart />
-      <SecondPart />
+            <FirstPart />
+     {/* <SecondPart />
       <ThirdPart />
       <FourthPart />
       <FifthPart />
