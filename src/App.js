5e3bb0ed9a3980c3835f64e2e813294c150aa49a
m-mcsim7 <m-mcsim7@yandex.by>
2022-02-11 11:11:31 +0300

    without link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 import Header from "./components/Header/Header";
 //import FirstPart from "./components/FirstPart/FirstPart";
