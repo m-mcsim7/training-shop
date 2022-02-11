16d2bde9063646ce084593a12ee6cd5b17b27a11
m-mcsim7 <m-mcsim7@yandex.by>
2022-02-11 12:22:24 +0300

    add

--- src/components/Header/Navigation.jsx
+++ src/components/Header/Navigation.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navigation({ items, onClick }) {
   return (
     <ul className="main__navigation">
       {items.map((name_navigation, index) => (
         <li key={index}>
-          <a href={name_navigation} onClick={() => onClick(name_navigation)}>
+          <Link to={name_navigation} onClick={() => onClick(name_navigation)}>
             {name_navigation}
-          </a>
+          </Link>
         </li>
       ))}
     </ul>
