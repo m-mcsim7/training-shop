ba7acf05039717d8b23523ded1304537141999c6
m-mcsim7 <m-mcsim7@yandex.by>
2022-02-21 13:59:16 +0300

    slider

--- src/components/Item/Slider.jsx
+++ src/components/Item/Slider.jsx
@@ -1,5 +1,4 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
@@ -11,34 +10,41 @@ import "swiper/css/navigation";
 
 import Photo_1 from "../../img/images/main_photo.jpg";
 
+//import Item_color_1 from "../../img/images/item_color1.jpg";
+//import Item_color_2 from "../../img/images/item_color2.jpg";
+//import Item_color_3 from "../../img/images/item_color3.jpg";
+//import Item_color_4 from "../../img/images/item_color4.jpg";
+
 function Slider(props) {
-
-const [firstSwiper, setFirstSwiper] = useState(null);
-const [secondSwiper, setSecondSwiper] = useState(null);
-//const [activeSlide, srtActiveSlide] = useState(true)
-
+  const [firstSwiper, setFirstSwiper] = useState(null);
+  const [secondSwiper, setSecondSwiper] = useState(null);
+//  const nav = ".item__slider-small-photo"
+  //const [activeSlide, srtActiveSlide] = useState(true)
 
   return (
-    <section className="slider_item_swiper"
-    data-test-id="product-slider">
+    <section className="slider_item_swiper" data-test-id="product-slider">
+      {/*<div className="item__slider-small-photo">
+       <img src={Photo_1} alt="baner" />
+       <img src={Photo_1} alt="baner" />
+       <img src={Photo_1} alt="baner" />
+       <img src={Photo_1} alt="baner" />
+       </div>*/}
       <Swiper
-      
         className="item__slider-small-photo mySwiper"
         navigation={true}
         modules={[Navigation, Controller]}
-        onSwiper={setFirstSwiper}
-        controller={{ control:secondSwiper }}
+          onSwiper={setFirstSwiper}
+          controller={{ control:secondSwiper }}
 
         navigation={{
-         nextEl: props.buttons.nextEl,
-         prevEl: props.buttons.prevEl
+          nextEl: props.buttons.nextEl,
+          prevEl: props.buttons.prevEl,
         }}
-      
-
         direction="vertical"
-        slidesPerView={3}
-      //  slidesPerGroup={1}
+        slidesPerView={4}
+        slidesPerGroup={1}
         spaceBetween={16}
+        loop= {true}
       >
         <SwiperSlide>
           <img src={Photo_1} alt="baner" />
@@ -52,18 +58,27 @@ const [secondSwiper, setSecondSwiper] = useState(null);
         <SwiperSlide>
           <img src={Photo_1} alt="baner" />
         </SwiperSlide>
-       </Swiper>
+        <SwiperSlide>
+          <img src={Photo_1} alt="baner" />
+        </SwiperSlide>
+      </Swiper>
 
       <Swiper
         className="item__slider-big-photo mySwiper"
         navigation={true}
         modules={[Navigation, Controller]}
-        onSwiper={setSecondSwiper}
-        controller={{ control:firstSwiper }}
-
-        
+          onSwiper={setSecondSwiper}
+          controller={{ control:firstSwiper }}
+          slidesPerGroup={1}
+          slidesPerView={1}
+          loop= {true}
+      //  thumbs={{
+      //    swiper: {nav}
+      //  }}
       >
-         
+        <SwiperSlide>
+          <img src={Photo_1} alt="baner" />
+        </SwiperSlide>
         <SwiperSlide>
           <img src={Photo_1} alt="baner" />
         </SwiperSlide>
