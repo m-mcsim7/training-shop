027a037b75e5368aff0a98083979770f10d09a10
m-mcsim7 <m-mcsim7@yandex.by>
2022-03-26 21:50:50 +0300

    useAddEmailMutation

--- src/components/FifthPart/FifthPart.jsx
+++ src/components/FifthPart/FifthPart.jsx
@@ -1,19 +1,52 @@
 import React from "react";
+import { useAddEmailMutation } from "../../redux/buttonEmail/reducer";
 import BigBannerWoman from "../../img/images/woman.png";
 import BigBannerMan from "../../img/images/man.png";
 import "./FifthPart.css";
 
 function FifthPart() {
+  const [email, setEmail] = React.useState("");
+  const [emailDirty, setEmailDirty] = React.useState(false);
+  const [emailError, setEmailError] = React.useState(
+    "E-mail не может быть пустым"
+  );
+  const [formValid, setFormValid] = React.useState(false);
+  const [addEmail, { isLoading, isError }] = useAddEmailMutation();
+
+  const handleAddEmail = async () => {
+    if (email) {
+      await addEmail({ mail: email }).unwrap();
+      setEmail('')
+    }
+  };
+
+  React.useEffect(() => {
+    if (emailError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [emailError]);
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+    const re =
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (!re.test(String(e.target.value).toLocaleLowerCase())) {
+      setEmailError("Некорректный e-mail");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  const blurHandler = () => setEmailDirty(true);
+
   return (
     <div>
       <div className="bigbanner">
         <div className="container">
           <div className="bigbanner__wrapper">
-            <img
-              className="bigbanner_woman"
-              src={BigBannerWoman}
-              alt="woman"
-            />
+            <img className="bigbanner_woman" src={BigBannerWoman} alt="woman" />
             <img className="bigbanner_man" src={BigBannerMan} alt="man" />
             <div className="bigbanner__contain">
               <p className="bigbanner__subtitle">Special Offer</p>
@@ -22,13 +55,27 @@ function FifthPart() {
                 <br />
                 and <span>Get 10% Off</span>
               </p>
-              <input
-                className="bigbanner__email"
-                type="email"
-                name="email-address"
-                placeholder="Enter your email"
-              />
-              <button className="bigbanner__button">Subscribe</button>
+              <form>
+                <input
+                  onBlur={(e) => blurHandler(e)}
+                  value={email}
+                  onChange={(e) => emailHandler(e)}
+                  className="bigbanner__email"
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email"
+                />
+                {emailDirty && emailError && (
+                  <div className="bigbanner__email-error">{emailError}</div>
+                )}
+                <button 
+                className="bigbanner__button" 
+                disabled={!formValid}
+                onClick={handleAddEmail}
+                >
+                  Subscribe
+                </button>
+              </form>
             </div>
           </div>
         </div>
---
+++ src/redux/buttonEmail/reducer.js
@@ -0,0 +1,16 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+export const emailApi = createApi({
+   reducerPath: 'emailApi',
+   baseQuery: fetchBaseQuery({baseUrl: 'https://training.cleverland.by/shop/email'}),
+   endpoints: (build) => ({
+      addEmail: build.mutation({
+         query: (body) => ({
+            method: 'POST',
+            body
+         }),
+      })
+   })
+});
+
+export const {useAddEmailMutation} = emailApi;
